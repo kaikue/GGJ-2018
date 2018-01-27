376ad9b529a952e9719313df596432406ea90bc3
kaikue <kvk@pobox.com>
2018-01-27 12:35:11 -0500

    Manimation, outro

---
+++ Assets/Scripts/Outro.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Outro : MonoBehaviour {
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Mouse0) ||
+				Input.GetKeyDown(KeyCode.Space) ||
+				Input.GetKeyDown(KeyCode.JoystickButton0))
+		{
+			Application.Quit();
+        }
+	}
+}
