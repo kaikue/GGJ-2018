60ecde1334d1d5ae29c21bfbe15c005aae7d3336
kaikue <kvk@pobox.com>
2018-01-26 21:38:15 -0500

    display remaining uninfected

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,28 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
 	public GameObject player;
 	public Camera cam;
+	public Text txt;
 
 	public bool UsingMouse = true;
 
 	private List<GameObject> infected;
 	private int playerIndex;
+	private List<GameObject> uninfected;
 
-	void Start ()
+	void Start()
 	{
+		uninfected = new List<GameObject>(GameObject.FindGameObjectsWithTag("Person"));
 		infected = new List<GameObject>();
 		AddInfected(player);
 		playerIndex = 0;
 		SetControl(player, true);
 	}
 	
-	void Update ()
+	void Update()
 	{
-		
+		txt.text = "Uninfected Remaining: " + uninfected.Count;
+	}
+
+	void FixedUpdate()
+	{
+		int numRemaining = uninfected.Count;
+		if (numRemaining == 0)
+		{
+			print("YOU WIN!!!");
+			//TODO win stuff
+		}
 	}
 
 	private void SetControl(GameObject personObj, bool controlling)
@@ -42,6 +56,7 @@ public class GameController : MonoBehaviour {
 	public void AddInfected(GameObject person)
 	{
 		infected.Add(person);
+		uninfected.Remove(person);
 	}
 
 	public void SwitchPlayer()
@@ -51,4 +66,5 @@ public class GameController : MonoBehaviour {
 		SetControl(infected[playerIndex], true);
 		print("Switch player " + playerIndex);
 	}
+
 }
