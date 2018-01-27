db38d865157207654f94867cbd10e3916b8b02d9
kaikue <kvk@pobox.com>
2018-01-26 19:28:00 -0500

    more movement stuff

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject player;
+	public Camera camera;
+
+	void Start ()
+	{
+		TakeControl(player);
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+	void TakeControl(GameObject player)
+	{
+		Person person = player.GetComponent<Person>();
+		person.Playable = true;
+		camera.transform.SetParent(player.transform);
+	}
+}
---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour {
+
+	public bool Infected;
+	public bool Playable;
+
+	private const float SPEED_MULT = 0.05f;
+
+	private Rigidbody2D rb;
+	private bool coughQueued = false;
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+		if (Playable && 
+			(Input.GetKeyDown(KeyCode.Mouse0) ||
+			 Input.GetKeyDown(KeyCode.Space) ||
+			 Input.GetKeyDown(KeyCode.JoystickButton0)))
+		{
+			coughQueued = true;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (Playable)
+		{
+			Vector2 vel = rb.velocity;
+
+			vel.x = Input.GetAxis("Horizontal") * SPEED_MULT;
+			vel.y = Input.GetAxis("Vertical") * SPEED_MULT;
+
+			rb.MovePosition(rb.position + vel);
+
+			if (coughQueued)
+			{
+				print("cough");
+				coughQueued = false;
+			}
+		}
+	}
+}
